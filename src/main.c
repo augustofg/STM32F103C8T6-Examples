f2b2c922084f73fb81d2aacb20da7497858be8a6
Augusto Fraga Giachero <augustofg96@gmail.com>
2015-09-26 18:50:29 -0300

    Better main.c template

--- src/main.c
+++ src/main.c
@@ -2,6 +2,11 @@
 
 int main()
 {
+	/* Enable all ports and Alternate Function clocks */
+	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPBEN |
+	    RCC_APB2ENR_IOPCEN | RCC_APB2ENR_IOPDEN | RCC_APB2ENR_AFIOEN;
+	/* Disable JNTRST and SWO (Free PB3 and PB4) */
+	AFIO->MAPR = AFIO_MAPR_SWJ_CFG_NOJNTRST;
 	while(1)
 	{
 		
